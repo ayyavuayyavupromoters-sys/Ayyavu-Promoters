779cf79e586c34dbdcf187826da9886bfc9f4e4c
ayyavuayyavupromoters-sys <ayyavu.ayyavupromoters@gmail.com>
2026-01-11 11:20:20 +0530

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://lgubloplgjhbmecgplpr.supabase.co'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://wdwhvextsqtszyftvihb.supabase.co'
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxndWJsb3BsZ2poYm1lY2dwbHByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE5ODgwMTAsImV4cCI6MjA3NzU2NDAxMH0.aiWLfVdZMU08uF-UzHjD9LeZuj14JVcEx78btarqGfQ'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
